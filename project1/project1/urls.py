7ba47553b32c4aa12069078fdfbd4facc88bf4b9
rex <niedao516@126.com>
2014-09-06 22:55:34 +0800

    add a app in setting.py and modify urlsw

--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'project1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/index/$', 'blog.views.index'),
 )
