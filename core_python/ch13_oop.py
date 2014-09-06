3877f68310140d1ba86aff924e48cd1148f41eb7
rex <niedao516@126.com>
2014-09-06 21:07:17 +0800

    commit local modify

--- core_python/ch13_oop.py
+++ core_python/ch13_oop.py
@@ -48,5 +48,17 @@ def TestAddrBookEntry():
     #EmplAddrBookEntry.updateEmail("003@gmail.com")
     print dao.__class__
 
+class HotelRoomCalc(object):
+    """hotel room calculator"""
+    def __init__(self, rt, sales=0.085, rm=0.1):
+        self.salesTax = sales
+        self.roomTax = rm
+        self.roomRate = rt
+
+    def calcTotal(self, days=1):
+        daily = round((self.roomRate*(1 + self.roomTax + self.salesTax)), 2)
+        return float(days) * daily
+
+
 if __name__ == "__main__":
     TestAddrBookEntry()
