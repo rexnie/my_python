63b59295601a92eb01dd1a5e17902629ee5c78b5
rex <niedao516@126.com>
2014-09-07 22:44:13 +0800

    admin the database, sqlite3

--- project_2_admin/blog/models.py
+++ project_2_admin/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+sex_choices = (
+    ('f', 'female'),
+    ('m', 'male'),
+)
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 30)
+    sex = models.CharField(max_length = 1, choices = sex_choices)
+
+    def __unicode__(self):
+        return self.name
