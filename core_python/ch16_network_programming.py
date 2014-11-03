4f280826ba9c28ee378b4b3336248d4a9faf3131
Your Name <niedaocai@niedaocai-NB.(none)>
2014-11-03 21:42:23 +0800

    [threading] use threading to communicate with client

--- core_python/ch16_network_programming.py
+++ core_python/ch16_network_programming.py
@@ -1,30 +1,59 @@
 #!/usr/bin/env python
 import socket as skt
 from time import ctime
+import threading
 
 BUFFER_SIZE = 1024
+
+class MyThread(threading.Thread):
+    """ tcp server threading main loop that communicate with client
+        tcp_client_socket: the socket that return from accept method
+        cli_addr: the address that return from accept method, a tuple
+    """
+    def __init__(self, tcp_client_socket, cli_addr):
+        super(MyThread, self).__init__()
+        self.tcp_client_socket = tcp_client_socket
+        self.cli_addr = cli_addr
+        self.loop = False
+    def run(self):
+        self.loop = True
+        while self.loop:
+            data = self.tcp_client_socket.recv(BUFFER_SIZE)
+            if not data:
+                break
+            self.tcp_client_socket.send('(%s) %s' % (ctime(), data))
+
+    def quit(self):
+        self.loop = False
 
 def run_tcp_server(host='', port=21567):
     server_addr = (host, port)
     tcp_server_socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM) 
     tcp_server_socket.bind(server_addr)
     tcp_server_socket.listen(5)
+    client_skt_addr_dict = {}
+    print 'waiting for connection...'
 
     try:
         while True:
-            print 'waiting for connetion...'
             tcp_client_socket, cli_addr = tcp_server_socket.accept()
-            print '...conneted from:', cli_addr
+            t = MyThread(tcp_client_socket, cli_addr)
+            client_skt_addr_dict[(tcp_client_socket, cli_addr)] = t
+            t.start()
+            print '=' * 20
+            for d in client_skt_addr_dict:
+                print client_skt_addr_dict[d]
 
-            while True:
-                data = tcp_client_socket.recv(BUFFER_SIZE)
-                if not data:
-                    break
-                tcp_client_socket.send('(%s) %s' % (ctime(), data))
     except (KeyboardInterrupt, EOFError), err:
-        print 'got error', err
+        print 'got error,cnt=', err, threading.active_count()
+        for d in client_skt_addr_dict:
+            t = client_skt_addr_dict[d]
+            print 'cnt=', threading.active_count()
+            t.quit()
+
         tcp_client_socket.close()
         tcp_server_socket.close()
+        print client_skt_addr_dict, threading.active_count()
 
 def run_tcp_client(host='localhost', port=21567):
     tcp_client_socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
@@ -75,7 +104,7 @@ def run_udp_client(host='localhost', port=21567):
         udp_client_socket.close()
 
 if __name__ == '__main__':
-    #run_tcp_server()
+    run_tcp_server()
     #run_tcp_client(host='10.241.53.79') # server ip
-    run_udp_server()
+    #run_udp_server()
     #run_udp_client(host='10.241.53.79') # server ip
