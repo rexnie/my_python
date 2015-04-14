7f4f9e52bec8ee540c0a7e5082bc41ae7c524b93
niedaocai <niedaocai@snda.com>
2015-04-14 23:45:02 +0800

    [adbtool] add other adb cmd for adbtool

--- snda_factary_test/adbtools.py
+++ snda_factary_test/adbtools.py
@@ -4,24 +4,27 @@ import os
 import time
 
 # absolute path of adb bin
-ADB = "/bin/adb"
+if os.name == 'posix':
+    ADB = "/bin/adb"
+else:
+    ADB='adb\\adb.exe'
 
 def connect_and_check():
     '''check adb devices connect or not'''
     adb = WrapCommand('%s devices' % ADB,shell=True)
     adb.start()
-   
-    i = 3
+    i = 5
     while i > 0 and adb.is_alive():
         i -= 1
-        time.sleep(0.5)
+        time.sleep(1)
     if adb.is_alive():
         adb.stop()
    
+    print 'adb111'
     adb.join()
-    #print adb.results
-    for line in adb.results[0].split('\n'):
-        if line.endswith("device"):
+    print adb.results
+    for line in adb.results[0].split(os.linesep):
+        if line.endswith("device") or line.endswith("recovery"):
             return True 
     return False
 
@@ -35,9 +38,26 @@ def connect_devices():
     '''check adb devices connect or not,with retry action'''
     if not connect_and_check():
         adb_kill_server()
-        time.sleep(0.5)
+        print 'adb112'
+        #time.sleep(1)
         return connect_and_check() 
     return True 
 
+def execute_adb_cmd(cmd='adb devices'):
+    ''' execute adb command
+        return a tuple (stdoutdata, stderrdata)
+    '''
+    idx=cmd.find('adb')
+    if idx == -1:
+       print 'must contain adb in cmd'
+       return False,False
+    cmd=cmd[idx+3 :]
+    print ADB,cmd
+    cmd2=WrapCommand('%s %s' % (ADB,cmd))
+    cmd2.start()
+    cmd2.join()
+    return cmd2.results
+
 if __name__ == '__main__':
     print connect_devices()
+    #print execute_adb_cmd(' adb pull /sdcard/t.wa .')
