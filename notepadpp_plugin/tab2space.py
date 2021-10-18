8a29fc47d82412383eccf4e4e2ef42631ae3cdf5
Rex Nie <niedao516@126.com>
2021-10-18 11:30:24 +0800

    [notepadpp_plugin] update

--- notepadpp_plugin/tab2space.py
+++ notepadpp_plugin/tab2space.py
@@ -1,23 +1,51 @@
+"""
+this plugin replace starting TABs to "|   ".
+open plugin>python script>show console to debug this script.
+
+sample doc:
+aaaa
+	bbbbbbb
+		ccccc
+			dddd
+				111111
+					111111
+						222222
+							33333
+								444444
+		eeeee
+	ffffff
+
+output result:
+aaaa
+|   bbbbbbb
+|   |   ccccc
+|   |   |   dddd
+|   |   |   |   111111
+|   |   |   |   |   111111
+|   |   |   |   |   |   222222
+|   |   |   |   |   |   |   33333
+|   |   |   |   |   |   |   |   444444
+|   |   eeeee
+|   ffffff
+
+"""
 import StringIO
 
 allText = editor.getText() #getSelText
 contextList = allText
-contextList += "\n\n"
+contextList += "\n"
 
 for line in StringIO.StringIO(allText):
     if line.find('\t') != 0: # not start with TAB
         contextList += line
     else:
+        # start with tab, replace TAB to "|   "
         tail=line.lstrip('\t')
         tmp=""
-        i=1
-        while line.rfind('\t', i) == i:
-            tmp ="    " + tmp
+        i=0
+        while line.find('\t', i) == i:
+            tmp ="|   " + tmp
             i+=1
-        if i == 1:
-            tmp = "|---"
-        else:
-            tmp += "|---"
         #print("line=%s,tmp=%s,tail=%s" % (line, tmp, tail))
         contextList += tmp + tail
 
